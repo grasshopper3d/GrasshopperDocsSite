b1643895a898ec95e0a59dce90431ad3dc0bf41d
Robin Rodricks <robinrodricks7@gmail.com>
2024-01-05 13:10:50 +0530

    ad notice

--- js/app-notice.js
+++ js/app-notice.js
@@ -12,7 +12,18 @@ $(document).ready(function() {
 			$('#help-notice').show();
 			
 			// animate it in
-			$('#help-notice').animate({ right: '0' }, 500);
+			$('#help-notice').animate({ right: '0' }, {
+				duration: 500,
+				complete: function() {
+					
+					// if ads are shown
+					if($('#carbonads').length != 0){
+						
+						// hide the notice
+						$('#help-notice').hide();
+					}
+				}
+			});
 		}
 		
 	}, 1000);
